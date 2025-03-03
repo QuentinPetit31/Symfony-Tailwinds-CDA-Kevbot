74d1f6d1ce153e6ba69dd183d4184e3bab98feb1
QuentinPetit31 <140169105+QuentinPetit31@users.noreply.github.com>
2025-03-03 16:14:29 +0100

    modif AccueilController

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -16,5 +16,40 @@ class AccueilController
         $resultat = $num1 + $num2;
         return new Response("<p>L’addition de $num1 et $num2 est égale au résultat : $resultat</p>");
     }
+
+
+    #[Route(path:'/calculatrice/{nbr1}/{nbr2}/{operateur}', name: 'app_accueil_calculatrice')]
+    public function calculatrice(mixed $nbr1, mixed $nbr2, string $operateur) : Response {
+        //Test si nbr1 ou nbr2 sont des nombres
+        if(!is_numeric($nbr1) || !is_numeric($nbr2)) {
+            $response = "<p>Les deux nombres ne sont pas numériques</p>";
+        }
+        //Sinon 
+        else {
+            switch ($operateur) {
+                case 'add':
+                    $response = "<p>L'addition de $nbr1 et $nbr2 est égale au résultat : " . ($nbr1 + $nbr2) . '</p>';
+                    break;
+                case 'sous':
+                    $response = "<p>La soustraction de $nbr1 et $nbr2 est égale au résultat : " . ($nbr1 - $nbr2) . '</p>';
+                    break;
+                case 'multi':
+                $response = "<p>La multiplication de $nbr1 et $nbr2 est égale au résultat : " . ($nbr1 * $nbr2) . '</p>';
+                    break;
+                case 'div':
+                    if($nbr2 === 0) {
+                        $response = "<p>Division par zéro impossible</p>";
+                    }else{
+                        $response = "<p>La division de $nbr1 et $nbr2 est égale au résultat : " . ($nbr1 / $nbr2) . '</p>';
+                    }
+                    break;
+                default:
+                    $response = "<p>Opérateur inconnu</p>";
+                    break;
+            }
+        }
+        return new Response($response);
+    }
+    
 }
     ?>
