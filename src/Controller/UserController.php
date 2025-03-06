ef3b51cb1d3d770bc060e721f5dc606b715aae8d
QuentinPetit31 <140169105+QuentinPetit31@users.noreply.github.com>
2025-03-06 10:20:29 +0100

    modifications UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AccountRepository;
 
-class UserController extends AbstractController
+final class UserController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_register')]
+    public function __construct(
+        private readonly AccountRepository $accountRepository
+    ) {}
+
+    #[Route('/register', name: 'app_user_register')]
     public function register(): Response
     {
-        return $this->render('user/register.html.twig', []);
+        return $this->render('user/register.html.twig');
+    }
+
+    #[Route('/login', name: 'app_user_login')]
+    public function login(): Response
+    {
+        return $this->render('user/login.html.twig');
+    }
+
+    #[Route('/accounts', name: 'app_user_accounts')]
+    public function showAllAccounts(): Response
+    {
+        return $this->render('user/accounts.html.twig', [
+            "accounts" => $this->accountRepository->findAll()
+        ]);
     }
 }
